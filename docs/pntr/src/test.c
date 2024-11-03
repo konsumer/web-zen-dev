1e1e3bf7d817df9787a4ac783060b52a34f43597
David Konsumer <konsumer@jetboystudio.com>
2024-11-03 02:54:43 -0800

    pntr demo

--- docs/pntr/src/test.c
+++ docs/pntr/src/test.c
@@ -10,15 +10,9 @@
 
 static FILE* fb;
 static pntr_image* screen;
-static int x[3] = {0,0,0};
-static int y[3] = {0,0,0};
-
-static int frameCount = 0;
-static time_t previousTime = 0;
-static time_t currentTime = 0;
-static int fps = 0;
 static pntr_font* defaultFont;
-static char fpsText[3];
+static char* fpsText;
+static int frameCount = 0;
 
 char* itoa(int value, char* result, int base) {
     // check that the base if valid
@@ -52,7 +46,10 @@ int rando() {
   return x ^= x << 10, x ^= x >> 13, (int)(x ^= x >> 10);
 }
 
-void updatePositions(){
+void drawCircles(){
+  static int x[3] = {0,0,0};
+  static int y[3] = {0,0,0};
+
   // only update every 20 frames
   if (frameCount % 20 == 0) {
     x[0] = rando() % SCREEN_WIDTH + 40;
@@ -62,9 +59,16 @@ void updatePositions(){
     y[1] = rando() % SCREEN_HEIGHT+ 40;
     y[2] = rando() % SCREEN_HEIGHT+ 40;
   }
+  pntr_draw_circle_fill(screen, x[0], y[0], 80, PNTR_BLUE);
+  pntr_draw_circle_fill(screen, x[1], y[1], 80, PNTR_GREEN);
+  pntr_draw_circle_fill(screen, x[2], y[2], 80, PNTR_RED);
 }
 
 void updateFps(){
+  static time_t previousTime = 0;
+  static time_t currentTime = 0;
+  static int fps = 0;
+
   frameCount++;
   currentTime = time(NULL);
   if ( currentTime - previousTime >= 1.0 ) {
@@ -72,20 +76,17 @@ void updateFps(){
     fps = frameCount;
     frameCount = 0;
   }
-  itoa(fps, &fpsText, 10);
-  pntr_draw_text(screen, defaultFont, &fpsText, 5, 5, PNTR_BLACK);
+  itoa(fps, fpsText, 10);
+  pntr_draw_text(screen, defaultFont, fpsText, 5, 5, PNTR_BLACK);
 }
 
 // this will be called by host when it's ready to update
 __attribute__((export_name("update")))
 void update() {
   pntr_clear_background(screen, PNTR_RAYWHITE);
-  pntr_draw_circle_fill(screen, x[0], y[0], 80, PNTR_BLUE);
-  pntr_draw_circle_fill(screen, x[1], y[1], 80, PNTR_GREEN);
-  pntr_draw_circle_fill(screen, x[2], y[2], 80, PNTR_RED);
-
   updateFps();
-  updatePositions();
+  drawCircles();
+  pntr_draw_text(screen, defaultFont, "If you can see this, it's working!", 180, SCREEN_HEIGHT/2, PNTR_BLACK);
 
   // write screen to framebuffer
   if (fb != NULL) {
@@ -94,6 +95,7 @@ void update() {
 }
 
 int main() {
+  fpsText = malloc(6);
   defaultFont = pntr_load_font_default();
 
   printf("Hi, from C.\n");
